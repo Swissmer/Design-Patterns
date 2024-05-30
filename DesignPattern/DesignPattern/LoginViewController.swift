947bce718cfae2b4a24fccf1371f10b3d42d435b
swissmer <danilpopyl@yandex.ru>
2024-05-30 22:55:13 +0300

    add empty project

---
+++ DesignPattern/DesignPattern/LoginViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginBtnClicked(_ sender: UIButton) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Design-Patterns
+
+в разработке...
